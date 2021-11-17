3e78b33cee9ee34e50dcd909efd76627fcf7237f
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2021-11-17 13:37:34 +0530

    exported the models to pickle files

--- flask_predictor_api/main.py
+++ flask_predictor_api/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from flask import Flask, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hellp World!"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/predictYield', methods=['POST'])
+def predictYield():
+    
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
